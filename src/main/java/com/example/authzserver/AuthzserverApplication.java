a5449dd49f322be825321a0e3d59f6bdb074018a
mojtaba <mojtaba98.mz@gmail.com>
2023-10-11 11:29:03 +0330

    init

---
+++ src/main/java/com/example/authzserver/AuthzserverApplication.java
@@ -0,0 +1,14 @@
+package com.example.authzserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AuthzserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthzserverApplication.class, args);
+	}
+
+}
